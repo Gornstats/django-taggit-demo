c3c462b23f87aa0c0672d7d661270432e4c952e2
Jeremy Gorniak <railspider@gmail.com>
2022-03-07 22:56:14 +1100

    Starting templates

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from taggit.models import Tag
+from core.models import Film
 
 # Create your views here.
 def index(request):
-    context = {}
+    films = Film.objects.prefetch_related('tags').all()
+    tags = Tag.objects.all()
+    context = {'films': films, 'tags':tags}
     return render(request, 'core/index.html', context)
