d4d8ccf31f1bafa78528526178ecda1e0f998570
Jeremy Gorniak <railspider@gmail.com>
2022-03-07 22:39:01 +1100

    Update admin.py to include tags
    Added model search and filtering by tag

--- mysite/core/admin.py
+++ mysite/core/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from core.models import Film
 
 # Register your models here.
-admin.site.register(Film)
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('name', 'director', 'get_tags',)
+    list_filter = ('tags', 'director',)
+    search_fields = ('name', 'director',)
+    
+    def get_tags(self, obj):
+        return ", ".join(o for o in obj.tags.names())
+
